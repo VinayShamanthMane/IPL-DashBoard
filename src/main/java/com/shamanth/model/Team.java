09e19b7e2fd6fb1229a8fa6d529b83f4cf1e8dd6
VinayShamanthMane <vinayshamanthmane@gmail.com>
2021-08-14 11:56:44 +0530

    Added Team API endpoints

---
+++ src/main/java/com/shamanth/model/Team.java
@@ -0,0 +1,76 @@
+package com.shamanth.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    @Transient
+    private List<Match> latestMatches;
+
+    public List<Match> getLatestMatches() {
+        return latestMatches;
+    }
+
+    public void setLatestMatches(List<Match> latestMatches) {
+        this.latestMatches = latestMatches;
+    }
+
+    public Team() {
+    }
+
+    public Team(String teamName, long e) {
+        this.teamName = teamName;
+        this.totalMatches = e;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    @Override
+    public String toString() {
+        return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
+    }
+}
